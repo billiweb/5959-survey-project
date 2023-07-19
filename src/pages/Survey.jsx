01eec7ca7e855b196ab998bd7ead6b6c8e43831c
heejin_song <song_heejin@naver.com>
2023-07-19 11:04:14 +0900

    next page button

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,13 +1,23 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { useState } from 'react';
+import { styled } from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function Survey() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { data, isLoading, error } = useQuery('survey', async () => {
     const response = await axios.get('http://localhost:3001/survey');
     return response.data;
   });
-  console.log(data);
+
+  console.log('>>', data);
+
+  const [page, setPage] = useState(1);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -16,13 +26,45 @@ function Survey() {
     return <div>Error occurred: {error.message}</div>;
   }
 
+  const nextButtonHandler = (post) => {
+    if (page !== 12) {
+      setPage(page + 1);
+    } else {
+      return navigate('/result');
+    }
+  };
+
   return (
-    <div>
+    <form>
       {data.map((post) => {
-        return <div key={post.id}>{post.answer1}</div>;
+        if (page === post.id)
+          return (
+            <PageContainer key={post.id}>
+              <p>{post.id}</p>
+              <p>{post.type}</p>
+              <p>질문 : {post.question}</p>
+              <Button onClick={() => nextButtonHandler(post)}>A : {post.answer1}</Button>
+              <Button onClick={() => nextButtonHandler(post)}>B : {post.answer2}</Button>
+            </PageContainer>
+          );
       })}
-    </div>
+    </form>
   );
 }
 
 export default Survey;
+
+const PageContainer = styled.div`
+  border: 1px solid black;
+  width: 350px;
+  padding: 10px;
+  margin: 20px auto;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 90%;
+  margin: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
