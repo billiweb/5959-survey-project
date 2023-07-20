87acd0c088b0d8406aa6192a74ac30f18f26bb9b
heejin_song <song_heejin@naver.com>
2023-07-20 22:14:39 +0900

    edit json server url for vercel

--- src/pages/LoginPage/Signup.jsx
+++ src/pages/LoginPage/Signup.jsx
@@ -26,8 +26,6 @@ function Signup() {
       const userCredential = await createUserWithEmailAndPassword(auth, Email, PW);
       const user = userCredential.user;
 
-      console.log('>>>', user);
-
       alert('회원가입 완료!');
       navigate('/login');
       // Dispatch
